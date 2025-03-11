64a16404d202a1386f2caa306e4819f1e53ff232
Jivit Rana <jivitrana87@gmail.com>
2025-03-11 23:18:23 +0530

    Create 1749. Maximum Absolute Sum of Any Subarray.py

---
+++ 1749. Maximum Absolute Sum of Any Subarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxAbsoluteSum(self, nums):
+        max_sum = 0
+        min_sum = 0
+        current_max = 0
+        current_min = 0
+
+        for num in nums:
+        
+            current_max = max(num, current_max + num)
+            max_sum = max(max_sum, current_max)
+
+    
+            current_min = min(num, current_min + num)
+            min_sum = min(min_sum, current_min)
+
+        return max(abs(max_sum), abs(min_sum))
+            
